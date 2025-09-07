5451d5e64b4752982f7cd21cc4fd3c8e41edb871
Karen Mkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-09-07 11:20:39 -0700

    Create point_location_receiver.py

---
+++ hw1/point_location_receiver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+"""
+this script receives ROS messages containing the 3D coordinated of a singel point and prints them out
+"""
+
+import rospy
+from geometry_msgs.msg import PointStamped
+
+rospy.init_node('receive_point_location')
+
+def process_point(data):
+    print("[{:.2f}, {:.2f}, {:.2f}]".format(data.point.x, data.point.y, data.point.z))
+
+rospy.Subscriber('/my_point', PointStamped, process_point)
+rospy.spin()
